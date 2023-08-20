5ee056005cc2842a77fe078ba13ed1a028614be3
foo7print <foo7print@gmail.com>
2023-08-20 16:27:08 +0900

    TwitterUIの作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MyApp(),
+    const MyApp(),
   );
 }
 
@@ -14,8 +14,70 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('タイトル'),
+          title: const Text(
+            'タイトル',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
         ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+            // backgroundImage: NetworkImage('画像のパス'),
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: const [
+                  Text('ユーザー名'),
+                  SizedBox(width: 8),
+                  Text('2023/08/20'),
+                ]
+              ),
+              const SizedBox(height: 4,),
+              const Text('ユーザーコメント'),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
